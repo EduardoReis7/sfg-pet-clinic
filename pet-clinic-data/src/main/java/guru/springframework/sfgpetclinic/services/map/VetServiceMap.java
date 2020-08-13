40ebfa60c2629db26355c65f5b10fde00c0821b8
EduardoReis7 <eduardodovigireis@gmail.com>
2020-08-13 20:01:55 -0300

    Adding specialities to vets on startup. Closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,19 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+	private final SpecialityService specialityService;
+
+	public VetServiceMap(SpecialityService specialityService) {
+		this.specialityService = specialityService;
+	}
 
 	@Override
 	public Set<Vet> findAll() {
@@ -19,17 +27,27 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 	public Vet findById(Long id) {
 		return super.findById(id);
 	}
-	
+
 	@Override
 	public Vet save(Vet vet) {
+		
+		if (vet.getSpecialitites().size() > 0) {
+			vet.getSpecialitites().forEach(speciality -> {
+				if(speciality.getId() == null) {
+					Speciality savedSpeciality = specialityService.save(speciality);
+					speciality.setId(savedSpeciality.getId());
+				}
+			});
+		}
+		
 		return super.save(vet);
 	}
-	
+
 	@Override
 	public void delete(Vet vet) {
 		super.delete(vet);
 	}
-	
+
 	@Override
 	public void deleteById(Long id) {
 		super.deleteById(id);
